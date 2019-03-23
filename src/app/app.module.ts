ad2a3580f163ebedd56d80cce78a771baa054fff
Maher farhat <farhatxyzmaher@gmail.com>
2019-03-23 11:21:34 +0100

    headerandfooter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule ,
-    FlexLayoutModule
+    FlexLayoutModule ,
+    MatToolbarModule ,
+    MatButtonModule ,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
